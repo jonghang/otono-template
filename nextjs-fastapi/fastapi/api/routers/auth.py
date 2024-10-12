e7dac555592746aa3206b5760a45830b62a2509b
jonghang <jonghang@gmail.com>
2024-10-12 22:17:58 +0800

    fixed hashed_password

--- nextjs-fastapi/fastapi/api/routers/auth.py
+++ nextjs-fastapi/fastapi/api/routers/auth.py
@@ -31,7 +31,7 @@ def authenticate_user(username: str, password: str, db):
     user = db.query(User).filter(User.username == username).first()
     if not user:
         return False
-    if not bcrypt_context.verify(password, user.hashed):
+    if not bcrypt_context.verify(password, user.hashed_password):
         return False
     return user
 
