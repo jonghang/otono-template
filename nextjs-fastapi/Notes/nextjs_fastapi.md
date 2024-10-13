297ee45cf96e5552210327eaf01ffac6a062a17a
Jong Hang Siong <jonghang@gmail.com>
2024-10-13 20:31:23 +0800

    fastapi

--- nextjs-fastapi/Notes/nextjs_fastapi.md
+++ nextjs-fastapi/Notes/nextjs_fastapi.md
@@ -4,3 +4,8 @@
 npx create-next-app nextjs-fastapi --example "https://github.com/digitros/nextjs-fastapi"
 ```
 
+# NPM Install
+
+```bash
+pnpm install axios bootstrap
+```
---
+++ nextjs-fastapi/app/context/AuthContext.js
@@ -0,0 +1,41 @@
+"use client";
+
+import React, {Children, createContext, useState} from "react";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const router = useRouter();
+
+    const login = async(username, password) => {
+        try{
+            const formData = new FormData();
+            formData.append('username', username);
+            formData.append('password', password);
+            const response = await axios.post('http://localhost:8000/auth/token', formData, {
+                headers: {'Content-Type':'application/x-www-form-urlencoded'},
+            });
+            axios.defaults.headers.common['Authorization'] = `Bearer ${response.data.access_token}`;
+            localStorage.setItem('token', response.data.access_token)
+        } catch (error) {
+            console.log(`Login Failed due to: ${error}`)
+        }
+    };
+
+    const logout = () => {
+        setUser(null);
+        delete axios.defaults.headers.common['Authorization'];
+        router.push('/login');
+    };
+
+    return (
+        <AuthContext.Provider value={{user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthContext;
